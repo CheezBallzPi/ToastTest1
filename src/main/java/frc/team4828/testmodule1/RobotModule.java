2175d945dcc3054563f358cafcd9cd47797e299b
Jackie Chen <cheezballz3.14@gmail.com>
2016-09-24 10:53:55 -0400

    Working Configurations
    For future reference: First create ModuleConfig object with config file as param, then use getObject("key") to get data.

--- src/main/java/frc/team4828/testmodule1/RobotModule.java
+++ src/main/java/frc/team4828/testmodule1/RobotModule.java
@@ -2,6 +2,8 @@ package frc.team4828.testmodule1;
 
 import jaci.openrio.toast.lib.log.Logger;
 import jaci.openrio.toast.lib.module.IterativeModule;
+import jaci.openrio.toast.lib.module.ModuleConfig;
+import sun.security.pkcs11.Secmod;
 
 public class RobotModule extends IterativeModule {
 
@@ -17,8 +19,10 @@ public class RobotModule extends IterativeModule {
 
     @Override
     public void robotInit() {
+        ModuleConfig config = new ModuleConfig("Toast.conf");
         Logger logger = new Logger("TestModule1", Logger.ATTR_DEFAULT);
         //TODO: Module Init
         logger.info("It's Alive!!!");
+        logger.info("The team name is: " + config.getObject("robot.team"));
     }
 }
