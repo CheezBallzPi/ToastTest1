9eba6349a1f23e4e89c41487a8f5edbd0ac1e5e0
Jackie Chen <cheezballz3.14@gmail.com>
2016-09-24 11:42:28 -0400

    Added TODOs

--- src/main/java/frc/team4828/testmodule1/RobotModule.java
+++ src/main/java/frc/team4828/testmodule1/RobotModule.java
@@ -1,9 +1,16 @@
 package frc.team4828.testmodule1;
 
+import jaci.openrio.toast.core.command.AbstractCommand;
+import jaci.openrio.toast.core.command.CommandBus;
+import jaci.openrio.toast.core.command.IHelpable;
+import jaci.openrio.toast.core.thread.Heartbeat;
+import jaci.openrio.toast.core.thread.HeartbeatListener;
 import jaci.openrio.toast.lib.log.Logger;
 import jaci.openrio.toast.lib.module.IterativeModule;
 import jaci.openrio.toast.lib.module.ModuleConfig;
 import sun.security.pkcs11.Secmod;
+
+import java.util.Arrays;
 
 public class RobotModule extends IterativeModule {
 
@@ -21,6 +28,10 @@ public class RobotModule extends IterativeModule {
     public void robotInit() {
         ModuleConfig config = new ModuleConfig("Toast.conf");
         Logger logger = new Logger("TestModule1", Logger.ATTR_DEFAULT);
+        //TODO: Heartbeat
+        //Heartbeat.add(skipped -> RobotModule.logger.info("Beep"));
+        //TODO: Commands
+        //TODO: USB Implementation
         //TODO: Module Init
         logger.info("It's Alive!!!");
         logger.info("The team name is: " + config.getObject("robot.team"));
